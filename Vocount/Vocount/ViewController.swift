13fcbe0d2980a76e3decb853679b27cd52206036
Juny0110 <wnsdyds403@naver.com>
2022-08-28 15:01:14 +0900

    Feat: Add Default Settings

---
+++ Vocount/Vocount/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Vocount
+//
+//  Created by 지준용 on 2022/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
