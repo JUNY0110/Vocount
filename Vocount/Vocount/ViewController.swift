d317e0008f59ebe7e34d6a8fbfc1d7ffa940e29d
Juny0110 <wnsdyds403@naver.com>
2022-08-28 16:17:35 +0900

    Feat: Add View & Layout

--- Vocount/Vocount/ViewController.swift
+++ Vocount/Vocount/ViewController.swift
@@ -9,8 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - View
+    
+    let roundButton: UIButton = {
+        return $0
+    }(UIButton())
+    
+    let buttonVstack: UIStackView = {
+        return $0
+    }(UIStackView())
+    
+    let buttonTitle: UILabel = {
+        $0.text = "이번 주 사용 어휘"
+        return $0
+    }(UILabel())
+    
+    let buttonContent: UILabel = {
+        return $0
+    }(UILabel())
+    
+    let mainTitle: UILabel = {
+        $0.text = "금주 사용 어휘 순위"
+        return $0
+    }(UILabel())
+    
+    let vStack: UIStackView = {
+        return $0
+    }(UIStackView())
+    
+    let hStack: UIStackView = {
+        return $0
+    }(UIStackView())
+    
+    let vocaRankingLabel: UILabel = {
+        return $0
+    }(UILabel())
+    
+    let vocaRankingCount: UILabel = {
+        return $0
+    }(UILabel())
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+
+    // MARK: - Layout
+
+    func layout() {
+        view.addSubview(roundButton)
+        roundButton.addSubview(buttonVstack)
+        buttonVstack.addArrangedSubview(buttonTitle)
+        buttonVstack.addArrangedSubview(buttonContent)
+        
+        view.addSubview(mainTitle)
+        view.addSubview(vStack)
+        vStack.addArrangedSubview(hStack)
+        hStack.addArrangedSubview(vocaRankingLabel)
+        hStack.addArrangedSubview(vocaRankingCount)
+        
+        view.anchor(top: view.topAnchor,
+                    left: view.leftAnchor,
+                    bottom: view.bottomAnchor,
+                    right: view.rightAnchor
+        )
+        view.setGradient(startColor: .backgroundMainColor, mediumColor: .subColor, lastColor: .mainColor)
     }
 }
 
