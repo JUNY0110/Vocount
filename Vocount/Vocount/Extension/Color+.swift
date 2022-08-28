e791dcb1aaa75c10e814c83c6e1711cb37ce730b
Juny0110 <wnsdyds403@naver.com>
2022-08-28 18:19:49 +0900

    Resource: Add Color for background

--- Vocount/Vocount/Extension/Color+.swift
+++ Vocount/Vocount/Extension/Color+.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 extension UIColor {
-    static let textColor: UIColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
-    static let mainColor: UIColor = UIColor(red: 108, green: 227, blue: 250, alpha: 1)
-    static let subColor: UIColor = UIColor(red: 102, green: 102, blue: 221, alpha: 0.5)
-    static let backgroundMainColor: UIColor = UIColor(red: 23, green: 23, blue: 23, alpha: 1)
+    static let softWhite: UIColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+    static let skyBlue: UIColor = UIColor(red: 108, green: 227, blue: 250, alpha: 1)
+    static let softBlue: UIColor = UIColor(red: 102, green: 102, blue: 221, alpha: 0.5)
+    static let topDarkBlue: UIColor = UIColor(red: 23, green: 23, blue: 23, alpha: 1)
+    static let mediumDarkBlue: UIColor = UIColor(red: 3, green: 8, blue: 53, alpha: 1)
+    static let mediumBlue: UIColor = UIColor(red: 9, green: 31, blue: 95, alpha: 1)
+    static let bottomBlue: UIColor = UIColor(red: 24, green: 61, blue: 155, alpha: 1)
     
     convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
         self.init(
@@ -24,12 +27,22 @@ extension UIColor {
 }
 
 extension UIView {
-    func setGradient(startColor: UIColor, mediumColor: UIColor, lastColor: UIColor) {
+    func setThirdGradient(startColor: UIColor, mediumColor: UIColor, lastColor: UIColor) {
         let gradient: CAGradientLayer = CAGradientLayer()
         gradient.colors = [startColor.cgColor, mediumColor.cgColor, lastColor.cgColor]
         gradient.locations = [0.0, 0.5, 1.0]
-        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 2.5)
+        gradient.frame = bounds
+        layer.addSublayer(gradient)
+    }
+    
+    func setFourthGradient(startColor: UIColor, secondColor: UIColor, thirdColor: UIColor, lastColor: UIColor) {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [startColor.cgColor, secondColor.cgColor, thirdColor.cgColor, lastColor.cgColor]
+        gradient.locations = [0.0, 0.25, 0.4, 1.0]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 2.5)
         gradient.frame = bounds
         layer.addSublayer(gradient)
     }
